b3c1393e8ef4bc674b090f795e72f48efd1314ca
MaxStetsenko <weawer458@gmail.com>
2020-09-10 17:47:35 +0300

    start temp

---
+++ constants/catalog.js
@@ -0,0 +1,39 @@
+const CATALOG = [
+	{
+		id: 'el1',
+		name: 'LOREM1 IPSUM DOLOR SIT AMET CONSE CTETUAR ADIPISICING ELIT',
+		img: 'https://i.ibb.co/w4bVwCY/catalog1.png',
+		price: 238,
+	},
+	{
+		id: 'el2',
+		name: 'LOREM2 IPSUM DOLOR SIT AMET CONSE CTETUAR ADIPISICING ELIT',
+		img: 'https://i.ibb.co/9q3HWsK/catalog2.png',
+		price: 268,
+	},
+	{
+		id: 'el3',
+		name: 'LOREM3 IPSUM DOLOR SIT AMET CONSE CTETUAR ADIPISICING ELIT',
+		img: 'https://i.ibb.co/Mg32GZd/catalog3.png',
+		price: 340,
+	},
+	{
+		id: 'el4',
+		name: 'LOREM4 IPSUM DOLOR SIT AMET CONSE CTETUAR ADIPISICING ELIT',
+		img: 'https://i.ibb.co/wpjpPjG/catalog4.png',
+		price: 340,
+	},
+	{
+		id: 'el5',
+		name: 'LOREM5 IPSUM DOLOR SIT AMET CONSE CTETUAR ADIPISICING ELIT',
+		img: 'https://i.ibb.co/1J6WnVV/catalog5.png',
+		price: 340,
+	},
+	{
+		id: 'el6',
+		name: 'LOREM6 IPSUM DOLOR SIT AMET CONSE CTETUAR ADIPISICING ELIT',
+		img: 'https://i.ibb.co/F4MDcKY/catalog6.png',
+		price: 340,
+	},
+];
+
